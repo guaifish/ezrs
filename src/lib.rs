f924a476bf86e73f3bf2deffde0f7d56a8f89a65
guaifish <guaifish@hotmail.com>
2020-09-16 16:39:53 +0800

    import read_input

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,5 @@
-pub use maplit::*;
+pub use maplit::{self, hashmap, hashset};
+pub use read_input::{
+    self,
+    shortcut::{input, simple_input},
+};
--- tests/maplit.rs
+++ tests/maplit.rs
@@ -15,7 +15,7 @@ fn test_hashmap() {
 
 #[test]
 fn test_btreemap() {
-    let m1 = btreemap! {
+    let m1 = maplit::btreemap! {
         'a' => 1,
         'b' => 2,
     };
@@ -36,9 +36,26 @@ fn test_hashset() {
 
 #[test]
 fn test_btreeset() {
-    let s1 = btreeset! { 'a', 'b' };
+    let s1 = maplit::btreeset! { 'a', 'b' };
     let mut s2 = BTreeSet::new();
     s2.insert('a');
     s2.insert('b');
     assert_eq!(s1, s2);
+}
+
+#[test]
+fn test_convert_args() {
+    let map1: HashMap<String, String> = maplit::convert_args!(hashmap!(
+        "a" => "b",
+        "c" => "d",
+    ));
+    let map2 = maplit::convert_args!(
+        keys = String::from,
+        values = String::from,
+        hashmap!(
+            "a" => "b",
+            "c" => "d",
+        )
+    );
+    assert_eq!(map1, map2);
 }
